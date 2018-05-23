2a835944e32252354f7df6f38f682a11f756664e
daqiqm@guilford.edu Daqiq <daqiqm@guilford.edu>
2018-05-23 09:47:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 var animal = 'dog'
-
+var yAnimal ='cat';
 function myAnimal() {
-  return myAnimal;
+  return animal;
 }
 
 function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
-  return myAnimal();
+  return yAnimal();
 }
 
 function add2(n) {
